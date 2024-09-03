eaed4d317a655de822dfc92a81874fb1d4a30b51
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-09-02 22:42:13 -0300

    configuracion de AOS sin funcionar

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 export default function Home() {
+
   
   return (
     <div className="sectionContainer grid grid-cols-2">
       <div className="flex flex-col gap-6 items-start justify-center">
-        <h1 className="h1 text-white" data-aos="fade-right">Cesar E. Zeballos</h1>
+        <h1 className="h1 text-white" >Cesar E. Zeballos</h1>
         <h2 className="body text-white">Full Stack Developer | UX/UI Designer | Business Administrator</h2>
         <p className="body text-white">My focus is on understanding user needs and translating them into visually appealing and technically sound solutions.</p>
         <div className="flex flex-row gap-4">
--- front/src/utils/AOSConfig.tsx
+++ front/src/utils/AOSConfig.tsx
@@ -6,6 +6,8 @@ import AOS from 'aos';
 export const AOSConfig = () => {
     useEffect(() => {
         AOS.init({
+          duration: 1200,
+          easing: 'ease-in-out',
           once: true
         });
       }, []);
