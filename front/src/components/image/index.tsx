da8416a62f8af9b76f6c5223c4fe5bff44687ba6
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-09-03 00:09:47 -0300

    imagen de perfil

--- front/src/components/image/index.tsx
+++ front/src/components/image/index.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image"
 
 
-const Profile: React.FC = () => {
+const Profile = ({imagen}: {imagen: string}) => {
 
     return (
-        <>
-        </>
+        <div className="flex flex-col items-center justify-center h-96">
+                <Image src={imagen} alt="profile" width={288} height={384} className="absolute z-10 rounded-3xl"/>
+                <div className="relative top-8 right-8 bg-lightBlue z-0 w-72 h-96 rounded-3xl"></div>
+        </div>
     )
 }
 
