23d2612f3fa95364737433a2415e09bfa8332cb4
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-09-12 23:11:37 -0300

    creacion del componente de contacto

---
+++ front/src/components/contact/index.tsx
@@ -0,0 +1,14 @@
+import { useRef, useState } from "react"
+
+
+export const Contact: React.FC = () => {
+    const contactRef = useRef<HTMLDivElement>(null)
+
+    
+
+    return (
+        <div>
+            magico contact
+        </div>
+    )
+}
